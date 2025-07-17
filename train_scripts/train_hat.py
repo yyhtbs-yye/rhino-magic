73a430aaa4162053ceede5f52d2c4009a8adf977
yyhtbs-yye <yye@research.ait.ie>
2025-07-17 10:42:58 +0100

    trained hat

--- train_scripts/train_hat.py
+++ train_scripts/train_hat.py
@@ -3,7 +3,7 @@ from trainer.rhino_train import main
 
 args_dict = {
     'config': 'configs/restoration/train_hat_ffhq_32_256_tiny.yaml',
-    'resume_from': 'work_dirs/hat_ffhq_32_256/run_1/boat_state_step=1094_epoch=1.pt',
+    'resume_from': None
 }
 
 args = types.SimpleNamespace(**args_dict)
